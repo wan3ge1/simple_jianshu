60473521bab7a5bfa6f183cfd58fb6aa142c148c
duzhen <duzhenf@yonyou.com>
2020-07-12 21:34:55 +0800

    create the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react'
+import GlobalStyle from './style'
+
+class App extends Component {
+  render () {
+    return (
+      <Fragment>
+        <GlobalStyle />
+        <div>hello world~</div>
+      </Fragment>
+    )
+  }
+}
+
+export default App
